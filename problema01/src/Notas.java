ea0254f9fb5ab4ce8e83ff197c51bfb8b4dc79b7
mathCesarSR <matheusforza123@gmail.com>
2025-06-10 21:03:02 -0300

    exercicio01

--- problema01/src/Notas.java
+++ problema01/src/Notas.java
@@ -1,11 +1,28 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Notas {
 
-    public static void main (String ice{}){
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
 
-        Scann
+        System.out.print("Digite a primeira nota: ");
+        double nota1 = sc.nextDouble();
 
+        System.out.print("Digite a segunda nota: ");
+        double nota2 = sc.nextDouble();
+
+        double notaFinal = nota1 + nota2;
+
+        System.out.printf("NOTA FINAL = %.1f%n", notaFinal);
+
+        if (notaFinal < 60.0) {
+            System.out.println("REPROVADO");
+        }
+
+        sc.close();
     }
-
 }
 
 
